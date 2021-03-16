3bc84ff8c1f9d6c75b8908d23d4e528d297b53c7
wangdf <zerotohero1996@163.com>
2021-03-16 10:46:06 +0800

    commit some leetcode Q&A

---
+++ src/LeetCode/everyday/GenerateMatrix.java
@@ -0,0 +1,52 @@
+package everyday;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 59. 螺旋矩阵 II
+ *
+ * @author Ernest
+ * @version 1.0, 16/3/2021
+ * @since 1.0.0
+ */
+public class GenerateMatrix {
+    /**
+     * 给你一个正整数 n ，生成一个包含 1 到 n*n 所有元素，
+     * 且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+     *
+     * @param n 表示数组为n的二维数组
+     * @return 返回生成的数组
+     */
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        int[][] direction = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int nowDireIndex = 0;
+        int count = 1;
+        int row = 0, col = 0;
+        while (count <= n * n) {
+            res[row][col] = count;
+            count++;
+            int nextRow = row + direction[nowDireIndex][0];
+            int nextCol = col + direction[nowDireIndex][1];
+            if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n || (res[nextRow][nextCol] != 0 && res[nextRow][nextCol]< count)) {
+                nowDireIndex = (nowDireIndex + 1) % 4;
+            }
+            row += direction[nowDireIndex][0];
+            col += direction[nowDireIndex][1];
+        }
+        return res;
+    }
+
+    @Test
+    public static void main(String[] args) {
+        GenerateMatrix g = new GenerateMatrix();
+        int[][] res = g.generateMatrix(3);
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res.length; j++) {
+                System.out.print(res[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/SwordFingerOffer/ReverseLeftWords.java
@@ -0,0 +1,26 @@
+package SwordFingerOffer;
+
+/**
+ * 剑指 Offer 58 - II. 左旋转字符串
+ *
+ * @author Ernest
+ * @version 1.0, 15/3/2021
+ * @since 1.0.0
+ */
+public class ReverseLeftWords {
+    /**
+     * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+     * 请定义一个函数实现字符串左旋转操作的功能。
+     * 比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+     *
+     * @param s 初始字符串
+     * @param n 左旋长度
+     * @return 返回旋转后的字符串
+     */
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder res = new StringBuilder();
+        res.append(s.substring(n, s.length() - 1));
+        res.append(s.substring(0, n));
+        return res.toString();
+    }
+}
