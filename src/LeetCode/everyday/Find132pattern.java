32fd661c18e53c970d75795b8ab0f499a640f2dc
wangdf <zerotohero1996@163.com>
2021-04-11 16:32:05 +0800

    commit some changes

---
+++ src/LeetCode/everyday/Find132pattern.java
@@ -0,0 +1,42 @@
+package everyday;
+
+/**
+ * 456. 132模式
+ *
+ * @author Ernest
+ * @version 1.0, 24/3/2021
+ * @since 1.0.0
+ */
+public class Find132pattern {
+    /**
+     * 给定一个整数序列：a1, a2, ..., an，一个132模式的子序列 ai, aj, ak 
+     * 被定义为：当 i < j < k 时，ai < ak < aj。
+     * 设计一个算法，当给定有 n 个数字的序列时，验证这个序列中是否含有132模式的子序列。
+     *
+     * @param nums 初始数组
+     * @return 返回是否包含132的子序列
+     */
+    public boolean find132pattern(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        find132(nums, l, r);
+        return false;
+    }
+
+    private boolean find132(int[] nums, int l, int r) {
+        if (nums[l] < nums[r]) {
+            for (int j = l; j < r; j++) {
+                if (nums[j] > nums[r]) {
+                    return true;
+                }
+            }
+        }
+        if (l < r) {
+            return find132(nums, l, r-1) || find132(nums, l+1, r);
+        }
+        return false;
+    }
+}
---
+++ src/SwordFingerOffer/CuttingRope.java
@@ -0,0 +1,31 @@
+package SwordFingerOffer;
+
+/**
+ * 剑指 Offer 14- I. 剪绳子
+ *
+ * @author Ernest
+ * @version 1.0, 23/3/2021
+ * @since 1.0.0
+ */
+public class CuttingRope {
+
+    /**
+     * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+     * 每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+     *
+     * @param n 绳子长度
+     * @return 返回最大的面积
+     */
+    public int cuttingRope(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            int max = 0;
+            for (int j = 1; j < i; j++) {
+                int cur = Math.max(j * dp[i - j], (i - j) * j);
+                max = Math.max(cur, max);
+            }
+            dp[i] = max;
+        }
+        return dp[n];
+    }
+}
